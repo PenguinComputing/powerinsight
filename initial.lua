ac396c0e07293c86364aa12615cbe770999718f0
root <root@beaglebone.(none)>
2014-12-16 09:23:12 +0000

    Starting work on Power Insight package of functions

--- initial.lua
+++ initial.lua
@@ -1,5 +1,27 @@
-RED   = { red=1.0, green=0.0, blue=0.0 }
-GREEN = { red=0.0, green=1.0, blue=0.0 }
-BLUE  = { red=0.0, green=0.0, blue=1.0 }
-T = { }
-J = { }
+-- Initialization of Lua environment
+T = { } -- Temperature channels
+J = { } -- Power rails
+E = { } -- Expansion connectors
+
+do -- Create pi (Power Insight) package
+local P = {}
+
+--[[
+local function private()
+    print("in private function")
+end
+
+local function foo()
+    print("Hello World!")
+end
+P.foo = foo
+
+local function bar()
+    private()
+    foo()
+end
+P.bar = bar
+]]--
+
+pi = P -- ie. return P
+end
