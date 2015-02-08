e5cac3dea9340d6c7ccfe3d199be8fd5a2e549ca
Philip Pokorny <ppokorny@penguincomputing.com>
2015-02-08 00:08:23 +0000

    Fixes based on actual testing
    Turns out you can't actually read or write more than 1 byte
    using read() and write() so drop the strings/length and just
    read and write single registers at a time.

--- pilib_i2c.c
+++ pilib_i2c.c
@@ -43,21 +43,19 @@ int pi_i2c_device( lua_State * L )
    return 0 ;
 }
 
-/* pi_i2c_read( bus, addr, len ) -- Read [len] bytes at addr
+/* pi_i2c_read( bus, addr ) -- Read byte at addr
  * @bus -- fd of spi to affect
  * @addr -- register address to select
- * @len -- number of bytes to read
  *
  * TODO: Maybe this should be an i2c-msg.  Unfortunately, we'd have to
- *   do a mess of data wrangling to use I2C_RDWR ioctl( )
+ *   do a mess of data wrangling to use I2C_RDWR ioctl( ), but we'd be
+ *   able to read/write more than 1 byte at a time.
  */
 int pi_i2c_read( lua_State * L )
 {
    int  fd ;
    int  addr ;
-   unsigned char  addr_buf ;
-   int  len ;
-   char *  buf ;
+   unsigned char  buf ;
    ssize_t  ret ;
 
    fd = luaL_checkint( L, 1 );
@@ -65,43 +63,32 @@ int pi_i2c_read( lua_State * L )
    addr = luaL_checkint( L, 2 );
    luaL_argcheck( L, addr == (addr & 0xff), 2, "invalid address" );
 
-   len = luaL_checkint( L, 3 );
-   luaL_argcheck( L, len > 0 && len <= 4096, 3, "invalid length [1-4096]" );
-
-   addr_buf = addr ;
-   ret = write( fd, &addr_buf, 1 );
+   buf = addr ;
+   ret = write( fd, &buf, 1 );
    if( ret == -1 ) {
       return luaL_error( L, "i2c_read: write(addr) error: %s", strerror(errno) );
    }
 
-   buf = malloc( len );
-   if( buf == NULL ) {
-      return luaL_error( L, "read buffer allocation failed" );
-   }
-   ret = read( fd, buf, len );
+   ret = read( fd, &buf, 1 );
    if( ret < 0 ) {
-      int  save_errno = errno ;
-      free( buf );
-      return luaL_error( L, "read failed: %s", strerror(save_errno) );
+      return luaL_error( L, "read failed: %s", strerror(errno) );
    }
 
-   lua_pushlstring( L, buf, len );
-   free( buf );
+   lua_pushnumber( L, buf );
    return 1 ;
 }
 
-/* pi_i2c_write( bus, addr, data ) -- Write [data] at addr
+/* pi_i2c_write( bus, addr, data ) -- Write data to addr
  * @bus -- fd of spi to affect
  * @addr -- register address to select
- * @data -- bytes to write
+ * @data -- byte to write
  */
 int pi_i2c_write( lua_State * L )
 {
    int  fd ;
    int  addr ;
-   const char *  data ;
-   size_t  len ;
-   char *  buf ;
+   int  data ;
+   unsigned char  buf[2] ;
    ssize_t  ret ;
 
    fd = luaL_checkint( L, 1 );
@@ -109,25 +96,18 @@ int pi_i2c_write( lua_State * L )
    addr = luaL_checkint( L, 2 );
    luaL_argcheck( L, addr == (addr & 0xff), 2, "invalid address" );
 
-   data = lua_tolstring( L, 3, &len );
-   luaL_argcheck( L, data != NULL && len > 0 && len <= 4096, 3, "invalid data" );
+   data = luaL_checkint( L, 3 );
+   luaL_argcheck( L, data == (data & 0xff), 3, "invalid data" );
 
-   buf = malloc( len +1 );
-   if( buf == NULL ) {
-      return luaL_error( L, "write buffer allocation failed" );
-   }
-   *buf = addr ;
-   memcpy( buf +1, data, len );
-   ret = write( fd, buf, len+1 );
+   buf[0] = addr ;
+   buf[1] = data ;
+   ret = write( fd, buf, 2 );
    if( ret < 0 ) {
-      int  save_errno = errno ;
-      free( buf );
-      return luaL_error( L, "write failed: %s", strerror(save_errno) );
-   } else if( ret != len+1 ) {
-      return luaL_error( L, "incomplete write: %d of %d", ret, len+1 );
+      return luaL_error( L, "write failed: %s", strerror(errno) );
+   } else if( ret != 2 ) {
+      return luaL_error( L, "incomplete write: %d of %d", ret, 2 );
    }
 
-   free( buf );
    return 0 ;
 }
 
